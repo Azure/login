fe71e4aee354a44276d897d7c2d467c42d4e068f
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-03-04 17:44:34 +0530

    Added changes for azure powershell login
    Changes in loginAzurePowerShell.ts
    
    using latest version
    
    Added getlatestazmodule version
    
    changes in runner info
    
    code refactor
    
    changes in loginAzurePowerShell
    
    code refactor
    
    Code refactor
    
    Code refactor
    
    added review comments
    
    changes in scriptbuilder
    
    changes in setmodulepath
    
    added paths in tsconfig.json
    
    Revert "added paths in tsconfig.json"
    
    This reverts commit cb2f4176bfc1e6603b6f7b2c4122f9327c913786.
    
    changes in action.yml
    
    changes in main
    
    added review comments
    
    Added changes for review comments
    
    Modified description in action.yml
    
    Added telemetry info
    
    Code refactor
    
    added review comments
    
    added review comments
    
    removed tslint from package.json
    
    added log in ServicePrincipalLogin
    
    added boolean for error log
    
    Added Unit tests (#15)
    
    * Added unit tests for Azure PowerShell
    
    * Added unit tests
    
    * changes in utils
    
    * removed babel
    
    * changed variable name of enable-PSSession
    
    * refactor
    
    * added ci.yml
    
    * changes in utils test
    
    making login calls silent (#19)
    
    Co-authored-by: Deepak Sattiraju <desattir@microsoft.com>
    
    update utils test (#16)
    
    * update utils test
    
    * update utils test
    
    * update serviceprincipallogin test

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,22 @@ import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 
 import { FormatType, SecretParser } from 'actions-secret-parser';
+import { ServicePrincipalLogin } from './PowerShell/ServicePrincipalLogin';
 
 var azPath: string;
 var prefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
+var azPSHostEnv = !!process.env.AZUREPS_HOST_ENVIRONMENT ? `${process.env.AZUREPS_HOST_ENVIRONMENT}` : "";
 
 async function main() {
-    try{
-        // Set user agent varable
+    try {
+        // Set user agent variable
+        var isAzCLISuccess = false;
         let usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex');
         let actionName = 'AzureLogin';
-        let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS_${actionName}_${usrAgentRepo}`;
+        let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
+        let azurePSHostEnv = (!!azPSHostEnv ? `${azPSHostEnv}+` : '') + `GITHUBACTIONS/${actionName}@v1_${usrAgentRepo}`;
         core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
+        core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azurePSHostEnv);
 
         azPath = await io.which("az", true);
         await executeAzCliCommand("--version");
@@ -25,30 +30,43 @@ async function main() {
         let servicePrincipalKey = secrets.getSecret("$.clientSecret", true);
         let tenantId = secrets.getSecret("$.tenantId", false);
         let subscriptionId = secrets.getSecret("$.subscriptionId", false);
+        const enableAzPSSession = core.getInput('enable-AzPSSession').toLowerCase() === "true";
         if (!servicePrincipalId || !servicePrincipalKey || !tenantId || !subscriptionId) {
             throw new Error("Not all values are present in the creds object. Ensure clientId, clientSecret, tenantId and subscriptionId are supplied.");
         }
-
-        await executeAzCliCommand(`login --service-principal -u "${servicePrincipalId}" -p "${servicePrincipalKey}" --tenant "${tenantId}"`);
-        await executeAzCliCommand(`account set --subscription "${subscriptionId}"`);
+        // Attempting Az cli login
+        await executeAzCliCommand(`login --service-principal -u "${servicePrincipalId}" -p "${servicePrincipalKey}" --tenant "${tenantId}"`, true);
+        await executeAzCliCommand(`account set --subscription "${subscriptionId}"`, true);
+        isAzCLISuccess = true;
+        if (enableAzPSSession) {
+            // Attempting Az PS login
+            console.log(`Running Azure PS Login`);
+            const spnlogin: ServicePrincipalLogin = new ServicePrincipalLogin(servicePrincipalId, servicePrincipalKey, tenantId, subscriptionId);
+            await spnlogin.initialize();
+            await spnlogin.login();
+        }
         console.log("Login successful.");    
     } catch (error) {
-        core.error("Login failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
+        if (!isAzCLISuccess) {
+            core.error("Az CLI Login failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows");
+        } else {
+            core.error(`Azure PowerShell Login failed. Please check the credentials. For more information refer https://aka.ms/create-secrets-for-GitHub-workflows"`);
+        }
         core.setFailed(error);
     } finally {
         // Reset AZURE_HTTP_USER_AGENT
         core.exportVariable('AZURE_HTTP_USER_AGENT', prefix);
+        core.exportVariable('AZUREPS_HOST_ENVIRONMENT', azPSHostEnv);
     }
 }
 
-async function executeAzCliCommand(command: string) {
+async function executeAzCliCommand(command: string, silent?: boolean) {
     try {
-        await exec.exec(`"${azPath}" ${command}`, [],  {}); 
+        await exec.exec(`"${azPath}" ${command}`, [],  {silent: !!silent}); 
     }
     catch(error) {
         throw new Error(error);
     }
 }
-
 
 main();
