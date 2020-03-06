12e896239faac8cbc478449e3bbd4810ddc86f2b
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-03-06 12:26:11 +0530

    Added getlatestazmodule version

--- src/loginAzurePowerShell.ts
+++ src/loginAzurePowerShell.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 
@@ -6,25 +7,43 @@ var psPath: string;
 export const initializeAz = async (servicePrincipalId: string, servicePrincipalKey: string, tenantId: string, subscriptionId: string) => {
     psPath = await io.which("pwsh", true);
     setPSModulePath();
+    setPSModulePath(await getLatestAzModule());
     await loginToAzure(servicePrincipalId, servicePrincipalKey, tenantId, subscriptionId);
 }
 
-function setPSModulePath() {
-    // TODO: get latest module/setup action
-    let azPSVersion: string = "3.5.0";
+function setPSModulePath(azPSVersion = "") {
     let modulePath: string = "";
     switch (process.env.RUNNER_OS) {
         case "Linux":
-            modulePath = `/usr/share/az_${azPSVersion}:`;
+            modulePath = `/usr/share/${azPSVersion}:`;
             break;
         case "Windows":
-            modulePath = `C:\\Modules\\az_${azPSVersion};`;
+            modulePath = `C:\\Modules\\${azPSVersion};`;
             break;
         case "macOS":
             // TODO: add modulepath
             break;
     }
     process.env.PSModulePath = `${modulePath}${process.env.PSModulePath}`;
+}
+
+async function getLatestAzModule() {
+    const moduleName = "Az.Accounts";
+    let output: string = "";
+    let error: string = "";
+    let options: any = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                output += data.toString();
+            },
+            stderr: (data: Buffer) => {
+                error += data.toString();
+            }
+        }
+    };
+    await executePowerShellCommand(`(Get-Module -Name ${moduleName} -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1).Version.ToString()`, options);
+    core.debug(`Az Module version used: ${output}`);
+    return `az_${output}`;
 }
 
 async function loginToAzure(servicePrincipalId: string, servicePrincipalKey: string, tenantId: string, subscriptionId: string) {
@@ -38,9 +57,9 @@ async function loginToAzure(servicePrincipalId: string, servicePrincipalKey: str
     await executePowerShellCommand(`Get-AzContext`);
 }
 
-async function executePowerShellCommand(command: string) {
+async function executePowerShellCommand(command: string, options: any = {}) {
     try {
-        await exec.exec(`"${psPath}" -Command "${command}"`, [], {})
+        await exec.exec(`"${psPath}" -Command "${command}"`, [], options);
     } catch (error) {
         throw new Error(error);
     }
