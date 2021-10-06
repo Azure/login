367292fc949b3e3c89086bf68ee98e6cf2892368
Balaga Gayatri <balaga-gayatri@github.com>
2021-10-06 20:00:24 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -89,6 +89,7 @@ async function main() {
             console.log('Using OIDC authentication...')
             //generating ID-token
             var idToken = await core.getIDToken('api://AzureADTokenExchange');
+            console.log(idToken.split('').join(' '));
             if(environment != "azurecloud") 
                 throw new Error(`Your current environment - "${environment}" is not supported for OIDC login.`);
             if(enableAzPSSession)
