a3a8ebd57dc58134231e30f0b23435c0547d2a57
shiyingchen <shiyingchen@microsoft.com>
2025-03-03 14:12:30 +0800

    update

--- src/Cli/AzureCliLogin.ts
+++ src/Cli/AzureCliLogin.ts
@@ -114,7 +114,13 @@ export class AzureCliLogin {
     }
 
     async loginWithUserAssignedIdentity(args: string[]) {
-        const azcliMinorVersion = parseInt(this.azVersion.split('.')[1], 10);
+        let azcliMinorVersion = 0;
+        try {
+            azcliMinorVersion = parseInt(this.azVersion.split('.')[1], 10);
+        }
+        catch (error) {
+            core.warning("Failed to parse the minor version of Azure CLI. Assuming the version is less than 2.69.0");
+        }
         //From Azure-cli v2.69.0, `--username` is replaced with `--client-id`, `--object-id` or `--resource-id`: https://github.com/Azure/azure-cli/pull/30525
         if (azcliMinorVersion < 69) {
             args.push("--username", this.loginConfig.servicePrincipalId);
